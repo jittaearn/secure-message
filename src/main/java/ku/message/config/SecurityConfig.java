e07067b44e17ef0ed376b0eb161aa901dbd2f301
jittaearn <jitta.k@ku.th>
2021-02-25 00:51:35 +0700

    Spring Security 4

--- src/main/java/ku/message/config/SecurityConfig.java
+++ src/main/java/ku/message/config/SecurityConfig.java
@@ -1,42 +1,34 @@
 package ku.message.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/home", "/css/**", "/js/**").permitAll()
-                .anyRequest().authenticated();
+   @Override
+   protected void configure(HttpSecurity http) throws Exception {
+       http
+               .authorizeRequests()
+               .antMatchers("/home", "/css/**", "/js/**").permitAll()
+               .anyRequest().authenticated()
 
-        http.formLogin()
-                .defaultSuccessUrl("/message", true)
-                .and().logout();
-    }
+       .and()
+               .exceptionHandling()
+               .authenticationEntryPoint(
+                          new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("usa")
-                .password(encoder().encode("usa"))
-                .roles("USER");
-    }
+       .and()
+               .oauth2Login()
+               .defaultSuccessUrl("/home").permitAll()
 
-    @Bean
-    public PasswordEncoder encoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-
+       .and()
+               .logout()
+               .logoutSuccessUrl("/home").permitAll();
+   }
 }
--- src/main/java/ku/message/controller/MessageController.java
+++ src/main/java/ku/message/controller/MessageController.java
@@ -2,6 +2,8 @@ package ku.message.controller;
 
 import ku.message.model.Message;
 import ku.message.repository.MessageRepository;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,29 +13,30 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class MessageController {
 
-    private MessageRepository repository;
+   private MessageRepository repository;
 
-    public MessageController(MessageRepository repository) {
-        this.repository = repository;
-    }
+   public MessageController(MessageRepository repository) {
+       this.repository = repository;
+   }
 
-    @GetMapping("/message")
-    public String getMessagePage(Model model) {
-        model.addAttribute("messages", repository.findAll());
-        return "message";
-    }
+   @GetMapping("/message")
+   public String getMessagePage(Model model) {
+       model.addAttribute("messages", repository.findAll());
+       return "message";
+   }
 
-    @GetMapping("/post")
-    public String getPostPage() {
-        return "post";
-    }
+   @GetMapping("/post")
+   public String getPostPage() {
+       return "post";
+   }
 
-    @PostMapping("/message")
-    public String postMessage(@ModelAttribute Message message, Model model) {
-        repository.save(message);
-        model.addAttribute("messages", repository.findAll());
-        return "redirect:message";
-    }
+   @PostMapping("/message")
+   public String postMessage(@ModelAttribute Message message, Model model, @AuthenticationPrincipal OAuth2User principal) {
+       message.setUser(principal.getAttribute("name"));
+       repository.save(message);
+       model.addAttribute("messages", repository.findAll());
+       return "redirect:message";
+   }
 
 
 }
